9962bdce5bfa20b0cc10d475d88f97021eb03bbc
Flavio Miamoto <hello@dorayaki.co>
2025-07-06 18:56:30 +0900

    nvim: update find command for telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -381,7 +381,11 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            find_command = { 'rg', '--files', '--hidden', '--glob', '!.git/*' },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),

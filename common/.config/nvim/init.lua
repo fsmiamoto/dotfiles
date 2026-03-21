a859de25555c3385a9ebb98d677e8589d389550f
Flavio Miyamoto <hello@dorayaki.co>
2026-03-21 19:12:36 +0900

    nvim: pin treesitter for now

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -883,6 +883,7 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
